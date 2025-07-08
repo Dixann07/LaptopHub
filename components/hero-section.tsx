dc7a904648750ac6f2a3b17d5966215a3b62d8a4
Dixann07 <146171789+Dixann07@users.noreply.github.com>
2025-07-08 17:48:10 +0545

    Update hero-section.tsx

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -84,7 +84,7 @@ export default function HeroSection() {
         <div
           className="absolute inset-0"
           style={{
-            backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fillRule='evenodd'%3E%3Cg fill='%23000000' fillOpacity='0.1'%3E%3Ccircle cx='30' cy='30' r='2'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
+            backgroundImage: `url("")`,
           }}
         ></div>
       </div>
